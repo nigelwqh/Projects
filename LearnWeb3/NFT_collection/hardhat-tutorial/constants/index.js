aa9ddadfdd6f451cad68da5b05c3a0446c4e95cd
Nigel Wong <nigelwong@Nigels-MacBook-Pro.local>
2022-11-19 11:56:14 +0800

    Added LearnWeb3 Sophomore course's NFT collection code

---
+++ LearnWeb3/NFT_collection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x9dbf708FD4C32591B5c6246157f6C93F58c2b825";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
